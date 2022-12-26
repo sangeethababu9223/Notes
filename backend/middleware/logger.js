ad31fe073503a0b50a4bfaa386651215ae458fa8
Nila <sangeetha9223@gmail.com>
2022-12-26 09:07:35 +0530

    Middleware

---
+++ backend/middleware/logger.js
@@ -0,0 +1,8 @@
+//@descr Log request to console
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+};
+
+module.exports = logger;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
 const notes = require("./routes/notes");
 dotenv.config({ path: "./config/config.env" });
 const app = express();
+
+app.use(logger);
 app.use("/api/v1/notes", notes);
 const PORT = process.env.PORT || 5000;
 app.listen(
