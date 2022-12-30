e5518cdfe7999e8fa773ca274a992a3aed421551
Nila <sangeetha9223@gmail.com>
2022-12-30 16:28:52 +0530

    basic CRUD operations

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,42 +1,90 @@
+const Notes = require("../models/Notes");
 // @desc    Get all notes
 // @route   GET api/v1/notes
 // @access  public
-exports.getNotes = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Get notes" });
+exports.getNotes = async (req, res, next) => {
+  try {
+    const notes = await Notes.find();
+    res.status(200).json({
+      success: true,
+      coun: notes.length,
+      data: notes,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc    Get single notes
 // @route   GET api/v1/notes/:id
 // @access  public
-exports.getNote = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Display note with id ${req.params.id}` });
+exports.getNote = async (req, res, next) => {
+  try {
+    const note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(400).json({
+        success: false,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: note,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc    Add note
 // @route   POST api/v1/notes/:id
 // @access  public
-exports.addNote = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Add note with id ${req.params.id}` });
+exports.addNote = async (req, res, next) => {
+  try {
+    const note = await Notes.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: note,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc    Update note
 // @route   PUT api/v1/notes/:id
 // @access  public
-exports.updateNote = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update note with id ${req.params.id}` });
+exports.updateNote = async (req, res, next) => {
+  try {
+    const note = await Notes.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!note) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: note });
+  } catch (err) {
+    return res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Delete note
 // @route   DELETE api/v1/notes/:id
 // @access  public
-exports.deleteNote = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete note with id ${req.params.id}` });
+exports.deleteNote = async (req, res, next) => {
+  try {
+    const note = await Notes.findByIdAndDelete(req.params.id);
+    if (!note) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
-const notes = require("./routes/notes");
+const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
+connectDB();
+const notes = require("./routes/notes");
 const app = express();
-
-app.use(logger);
+app.use(express.json());
 app.use("/api/v1/notes", notes);
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
 );
+
+// Handle Unhandles promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
