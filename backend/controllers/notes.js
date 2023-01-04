d2c673247c4175a3052d118ba283bcd914d502ad
Nila <sangeetha9223@gmail.com>
2023-01-04 22:51:33 +0530

    Error handler

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,8 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const Notes = require("../models/Notes");
-// @desc    Get all notes
-// @route   GET api/v1/notes
-// @access  public
+
 exports.getNotes = async (req, res, next) => {
   try {
     const notes = await Notes.find();
@@ -12,15 +10,10 @@ exports.getNotes = async (req, res, next) => {
       data: notes,
     });
   } catch (err) {
-    res.status(400).json({
-      success: false,
-    });
+    next(err);
   }
 };
 
-// @desc    Get single notes
-// @route   GET api/v1/notes/:id
-// @access  public
 exports.getNote = async (req, res, next) => {
   try {
     const note = await Notes.findById(req.params.id);
@@ -34,16 +27,10 @@ exports.getNote = async (req, res, next) => {
       data: note,
     });
   } catch (err) {
-    next(new ErrorResponse(`Note with id ${req.params.id} not found`, 404));
-    // res.status(400).json({
-    //   success: false,
-    // });
+    next(err);
   }
 };
 
-// @desc    Add note
-// @route   POST api/v1/notes/:id
-// @access  public
 exports.addNote = async (req, res, next) => {
   try {
     const note = await Notes.create(req.body);
@@ -52,15 +39,10 @@ exports.addNote = async (req, res, next) => {
       data: note,
     });
   } catch (err) {
-    res.status(400).json({
-      success: false,
-    });
+    next(err);
   }
 };
 
-// @desc    Update note
-// @route   PUT api/v1/notes/:id
-// @access  public
 exports.updateNote = async (req, res, next) => {
   try {
     const note = await Notes.findByIdAndUpdate(req.params.id, req.body, {
@@ -68,25 +50,26 @@ exports.updateNote = async (req, res, next) => {
       runValidators: true,
     });
     if (!note) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Note with id ${req.params.id} not found`, 404)
+      );
     }
     res.status(200).json({ success: true, data: note });
   } catch (err) {
-    return res.status(400).json({ success: false });
+    next(err);
   }
 };
 
-// @desc    Delete note
-// @route   DELETE api/v1/notes/:id
-// @access  public
 exports.deleteNote = async (req, res, next) => {
   try {
     const note = await Notes.findByIdAndDelete(req.params.id);
     if (!note) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Note with id ${req.params.id} not found`, 404)
+      );
     }
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
