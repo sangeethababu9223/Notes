734e4c36397ae3eee3da792bdf2e4ca79cbebf2f
Nila <sangeetha9223@gmail.com>
2023-01-26 17:52:32 +0530

    Emotion styles

--- src/components/notes/addNote/addNote.js
+++ src/components/notes/addNote/addNote.js
@@ -4,12 +4,16 @@ import "./addNote.scss";
 const Button = styled.button`
   color: ${(props) => props.primary ?? "hotpink"};
 `;
+const Container = styled.div((props) => ({
+  display: "flex",
+  flexDirection: props.column || "row",
+}));
 const AddNote = () => {
   return (
-    <div className='add-note__container'>
+    <Container className='add-note__container' column='column'>
       <h1>Add Note</h1>
       <Button primary='green'>Add</Button>
-    </div>
+    </Container>
   );
 };
 
